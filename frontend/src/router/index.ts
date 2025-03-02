38ad5d990fe357e5f5b6ebbc828e266ec4537c07
Rouge2123 <112276238+Rouge2123@users.noreply.github.com>
2025-03-02 20:18:54 +0100

    Added tailwind config file and started the inputs for the quiz

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,23 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import GenderSelection from '../views/GenderSelection.vue'
+import UserDetails from '../views/UserDetails.vue'
+import ActivityLevel from '../views/ActivityLevel.vue'
+
+const routes = [
+  { path: '/', component: GenderSelection },
+  { path: '/details', component: UserDetails },
+  { path: '/activity', component: ActivityLevel },
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+  history: createWebHistory(),
+  routes,
 })
 
 export default router
+
+// Default code
+// import { createRouter, createWebHistory } from 'vue-router'
+// import HomeView from '../views/HomeView.vue'
+
+// const router = createRouter({
+//   history: createWebHistory(import.meta.env.BASE_URL),
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'home',
+//       component: HomeView,
+//     },
+//     {
+//       path: '/about',
+//       name: 'about',
+//       // route level code-splitting
+//       // this generates a separate chunk (About.[hash].js) for this route
+//       // which is lazy-loaded when the route is visited.
+//       component: () => import('../views/AboutView.vue'),
+//     },
+//   ],
+// })
+
+// export default router
