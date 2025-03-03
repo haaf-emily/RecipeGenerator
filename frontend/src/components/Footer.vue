1a1603a084565b3a347297dc5667b8a998b1befa
Rouge2123 <112276238+Rouge2123@users.noreply.github.com>
2025-03-03 19:05:38 +0100

    Added footer

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="py-4 text-center text-gray-400 dark:text-gray-400">
+    Erstellt von Emily Haaf, Iliana Schotter, Roxanne Collett und Theresia Deubert
+  </footer>
+</template>
